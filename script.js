eecef06d0a6112ff42deb7c14386228327f6ef4b
iamwatchdogs <shamith301102@gmail.com>
2023-05-14 13:41:02 +0530

    Added unit conversion DOM logic

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-const inputValue = document.getElementById('from-value');
-const outputValue = document.getElementById('to-value');
-
-let from = document.querySelector('.from select');
-let to = document.querySelector('.to select');
-
-let fromOption, toOption;
-
-from.addEventListener('change', () => {
-    fromOption = from.options[from.selectedIndex].value;
-    console.log('from-option: '+ fromOption);
-});
-
-to.addEventListener('change', () => {
-    toOption = to.options[to.selectedIndex].value;
-    console.log('to-option: '+toOption);
-});
-
-inputValue.addEventListener('input', () => { outputValue.value = inputValue.value});
-
 // ----------------------------------------
 //           Unit Toggler Logic 
 // ----------------------------------------
 
 const selector = document.querySelectorAll('.selector');
 const displayBoxes = document.querySelectorAll('.display-box');
+let selectedUnitType = 'length';
 
 selector.forEach(selector => {
     selector.addEventListener('click', () => {
@@ -32,5 +13,50 @@ selector.forEach(selector => {
         });
         const displayBoxId = selector.getAttribute('data-target');
         document.getElementById(displayBoxId).style.display = 'grid';
+        selectedUnitType = displayBoxId;
     } );
 });
+
+// ----------------------------------------
+//     Unit Conversion I/O DOM Logic 
+// ----------------------------------------
+
+const inputValue = document.getElementById('from-value');
+const outputValue = document.getElementById('to-value');
+let fromOption, toOption;
+
+displayBoxes.forEach( displayBox => {
+    const from = displayBox.querySelector('.from select');
+    const to = displayBox.querySelector('.to select');
+
+    from.addEventListener('change', () => {
+        fromOption = from.options[from.selectedIndex].value;
+    });
+
+    to.addEventListener('change', () => {
+        toOption = to.options[to.selectedIndex].value;
+    });
+} );
+
+inputValue.addEventListener('input', (selectedUnitType) => {
+
+    // Type Casting input sting value to Float
+    const value = parseFloat(inputValue.value);
+
+    // For executing respective Unit Conversion
+    if (selectedUnitType == 'length') {
+        outputValue.value = convertLength(value);
+    } else if (selectedUnitType == 'temp') {
+        outputValue.value = convertTemp(value);
+    } else if (selectedUnitType == 'area') {
+        outputValue.value = convertArea(value);
+    } else if (selectedUnitType == 'volume') {
+        outputValue.value = convertVolume(value);
+    } else if (selectedUnitType == 'weight') {
+        outputValue.value = convertWeight(value);
+    } else if (selectedUnitType == 'time') {
+        outputValue.value = convertTime(value);
+    } else {
+        outputValue.value = 'Invalid `inputValue.value`';
+    }
+} );
